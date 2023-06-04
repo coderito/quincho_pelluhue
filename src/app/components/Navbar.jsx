1264b97f24ec1cc2082662ec47edfe90d4fedb61
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-06-03 21:14:59 -0400

    agregando funcionalidad al navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,26 +1,42 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
+
+// fuente
+import { Lobster_Two } from "next/font/google";
+
+const lobster = Lobster_Two({
+    subsets: ['latin'],
+    weight: ['400'],
+  });
 
 export default function Navbar() {
   const [menu, setMenu] = useState(false);
 
   const handleMenu = () => {
-    console.log(menu)
+    console.log(menu);
     setMenu(!menu);
   };
 
+  // navigation next router
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const estilosNavegacion =
+    "block cursor-pointer py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent text-blue-700 md:p-0";
+
   return (
     <>
-      <nav class="bg-white border-gray-200 dark:bg-gray-900 fixed w-full shadow-sm shadow-slate-100">
-        <div class="max-w-screen-lg flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" class="flex items-center">
+      <nav className="bg-[#06181c] border-gray-200 fixed w-full shadow-sm shadow-slate-100 z-50">
+        <div className="max-w-screen-lg flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="https://flowbite.com/" className="flex items-center">
             <img
               src="img/icon/logo.png"
-              class="h-12 w-12 mr-3"
+              className="h-12 w-12 mr-3 bg-white rounded-full"
               alt="Flowbite Logo"
             />
-            <span class="self-center tracking-wide text-md md:text-2xl font-bold text-[#946768] whitespace-nowrap dark:text-white">
+            <span className={`${lobster.className} self-center tracking-widest text-md md:text-2xl whitespace-nowrap text-white`}>
               El Quincho Pelluhue
             </span>
           </a>
@@ -28,58 +44,90 @@ export default function Navbar() {
             data-collapse-toggle="navbar-default"
             type="button"
             onClick={handleMenu}
-            class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-default"
             aria-expanded="false"
           >
-            <span class="sr-only">Open main menu</span>
+            <span className="sr-only">Open main menu</span>
             <svg
-              class="w-6 h-6"
+              className="w-6 h-6"
               aria-hidden="true"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
           </button>
-          <div class={`${menu ? "" : "hidden"} w-full md:block md:w-auto text-center`} id="navbar-default">
-            <ul class="font-medium text-xs flex flex-col p-4 md:p-0 mt-4 tracking-widest border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <div
+            className={`${
+              menu ? "" : "hidden"
+            } w-full md:block md:w-auto text-center`}
+            id="navbar-default"
+          >
+            <ul className="font-medium text-xs flex flex-col p-4 md:p-0 mt-4 md:gap-2 gap-3 tracking-widest border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
               <li>
                 <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  className={
+                    pathname === "/"
+                      ? estilosNavegacion
+                      : "hover:text-blue-500 text-white cursor-pointer"
+                  }
                   aria-current="page"
+                  onClick={() => router.push("/")}
                 >
                   Inicio
                 </a>
               </li>
               <li>
                 <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className={
+                    pathname === "/menu"
+                      ? estilosNavegacion
+                      : "hover:text-blue-500 text-white cursor-pointer"
+                  }
+                  onClick={() => router.push("/menu")}
                 >
-                  Menu
+                  Menú 
                 </a>
               </li>
               <li>
                 <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className={
+                    pathname === "/galeria"
+                      ? estilosNavegacion
+                      : "hover:text-blue-500 text-white cursor-pointer"
+                  }
+                  onClick={() => router.push("/galeria")}
                 >
-                  Galeria
+                  Galería
                 </a>
               </li>
               <li>
                 <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className={
+                    pathname === "/casas"
+                      ? estilosNavegacion
+                      : "hover:text-blue-500 text-white cursor-pointer"
+                  }
+                  onClick={() => router.push("/casas")}
                 >
-                  Cabanias
+                  Cabañas
+                </a>
+              </li><li>
+                <a
+                  className={
+                    pathname === "/contacto"
+                      ? estilosNavegacion
+                      : "hover:text-blue-500 text-white cursor-pointer"
+                  }
+                  onClick={() => router.push("/contacto")}
+                >
+                  Contacto
                 </a>
               </li>
             </ul>
