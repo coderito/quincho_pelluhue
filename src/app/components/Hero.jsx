9ee595e919cd68dff76b4d2e8465a528765d5df2
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-06-05 19:40:39 -0400

    reemplazando class por className

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -8,16 +8,16 @@ const playfair = Playfair_Display({
 
 export default function Hero() {
   return (
-    <div
-      class="flex items-center justify-center h-full md:h-screen"
+    <div  
+      className="pt-28 md:pt-36 lg:pt-10 flex items-center justify-center h-full md:h-screen bg-[#eaeaea]"
     >
       <section>
-        <div class="grid max-w-screen-lg mx-auto lg:grid-cols-12">
-          <div class="mr-auto place-self-center flex flex-col gap-4 lg:col-span-7">
+        <div className="grid max-w-screen-lg mx-auto lg:grid-cols-12">
+          <div className="mr-auto place-self-center flex flex-col gap-4 lg:col-span-7">
             <div className="flex items-center justify-center md:justify-normal">
               <svg
                 aria-hidden="true"
-                class="w-6 h-6 text-yellow-500"
+                className="w-6 h-6 text-yellow-500"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -28,41 +28,40 @@ export default function Hero() {
                   clip-rule="evenodd"
                 ></path>
               </svg>
-              <p class="tracking-widest uppercase text-xs">
+              <p className="tracking-widest uppercase text-xs">
                 Nuestro restaurant
               </p>
             </div>
 
             <div className={playfair.className}>
-              <div class="flex flex-col">
-                <h1 class="max-w-2xl font-bold tracking-wider leading-none md:text-5xl xl:text-6xl">
+              <div className="flex flex-col items-center md:items-start">
+                <h1 className="max-w-2xl font-extrabold tracking-wider leading-none text-4xl md:text-5xl xl:text-6xl">
                   EL QUINCHO
                 </h1>
               </div>
             </div>
-            <span className="text-gray-600 text-sm font-">Todo el año</span>
 
-            <div>
-            <p class="mb-6 font-medium tracking-wider [word-spacing:0.1rem] text-sm pr-24">
-              Años de dedicación en la búsqueda de una identidad propia nos
-              permiten compartir nuestra exquisita variedad gastronómica y
-              compartir la experiencia de nuestro aprendizaje. En nuestro
-              Restorán podrá degustar de la mas fresca y sabrosa cocina de
-              nuestro mar en sus recorridos por nuestro litoral. Esta tierra es
-              tierra que encanta especialmente por la acogida de su gente.
-              Queremos que tu estadía sea una hermosa experiencia de descanso y
-              un buen recuerdo que te invite siempre a volver.
-            </p>
-            <button className="bg-yellow-500 w-60 py-3 rounded-sm shadow-md uppercase text-sm shadow-gray-900">
-              Ver Carta
-            </button>
+            <div classNam="flex justify-center">
+              <p className="mb-6 font-medium tracking-wider [word-spacing:0.1rem] text-sm px-4 md:px-0 text-center md:text-start md:pr-24">
+                Años de dedicación en la búsqueda de una identidad propia nos
+                permiten compartir nuestra exquisita variedad gastronómica y
+                compartir la experiencia de nuestro aprendizaje. En nuestro
+                Restorán podrá degustar de la mas fresca y sabrosa cocina de
+                nuestro mar en sus recorridos por nuestro litoral. Esta tierra es
+                tierra que encanta especialmente por la acogida de su gente.
+                Queremos que tu estadía sea una hermosa experiencia de descanso y
+                un buen recuerdo que te invite siempre a volver.
+              </p>
+              <button className="bg-yellow-500 md:w-60 py-3 w-full rounded-sm shadow-md uppercase text-sm shadow-gray-900">
+                Ver Carta
+              </button>
             </div>
           </div>
-          <div class="lg:col-span-5">
+          <div className="lg:col-span-5">
             <img
               src="img/inicio/hero.jpg"
               alt="mockup"
-              class="rounded-sm object-cover h-96 shadow-xl"
+              className="rounded-sm object-cover h-96 shadow-xl"
             />
           </div>
         </div>
