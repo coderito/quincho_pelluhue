2dc6ef24303cc3e19ee2969669a0825297fd7167
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-06-05 20:30:32 -0400

    agregando paddings

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Playfair_Display} from "next/font/google";
+import { Playfair_Display } from "next/font/google";
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
@@ -8,13 +8,11 @@ const playfair = Playfair_Display({
 
 export default function Hero() {
   return (
-    <div  
-      className="pt-28 md:pt-36 lg:pt-10 flex items-center justify-center h-full md:h-screen bg-[#eaeaea]"
-    >
+    <div className="pt-28 md:pt-36 lg:pt-10 flex items-center justify-center h-full md:h-screen bg-[#eaeaea]">
       <section>
         <div className="grid max-w-screen-lg mx-auto lg:grid-cols-12">
           <div className="mr-auto place-self-center flex flex-col gap-4 lg:col-span-7">
-            <div className="flex items-center justify-center md:justify-normal">
+            <div className="flex items-center px-2 md:px-0 mt-12">
               <svg
                 aria-hidden="true"
                 className="w-6 h-6 text-yellow-500"
@@ -28,33 +26,35 @@ export default function Hero() {
                   clip-rule="evenodd"
                 ></path>
               </svg>
-              <p className="tracking-widest uppercase text-xs">
+              <p className="tracking-widest uppercase text-sm text-gray-800">
                 Nuestro restaurant
               </p>
             </div>
 
             <div className={playfair.className}>
-              <div className="flex flex-col items-center md:items-start">
-                <h1 className="max-w-2xl font-extrabold tracking-wider leading-none text-4xl md:text-5xl xl:text-6xl">
+              <div className="flex flex-col">
+                <h1 className="max-w-2xl px-4 md:px-0 font-extrabold tracking-wider leading-none text-5xl xl:text-6xl">
                   EL QUINCHO
                 </h1>
               </div>
             </div>
 
-            <div classNam="flex justify-center">
-              <p className="mb-6 font-medium tracking-wider [word-spacing:0.1rem] text-sm px-4 md:px-0 text-center md:text-start md:pr-24">
+            <div>
+              <p className="mb-6 font-medium pt-5 tracking-wider [word-spacing:0.1rem] text-sm px-4 md:px-0 md:pr-24">
                 Años de dedicación en la búsqueda de una identidad propia nos
                 permiten compartir nuestra exquisita variedad gastronómica y
                 compartir la experiencia de nuestro aprendizaje. En nuestro
                 Restorán podrá degustar de la mas fresca y sabrosa cocina de
-                nuestro mar en sus recorridos por nuestro litoral. Esta tierra es
-                tierra que encanta especialmente por la acogida de su gente.
-                Queremos que tu estadía sea una hermosa experiencia de descanso y
-                un buen recuerdo que te invite siempre a volver.
+                nuestro mar en sus recorridos por nuestro litoral. Esta tierra
+                es tierra que encanta especialmente por la acogida de su gente.
+                Queremos que tu estadía sea una hermosa experiencia de descanso
+                y un buen recuerdo que te invite siempre a volver.
               </p>
-              <button className="bg-yellow-500 md:w-60 py-3 w-full rounded-sm shadow-md uppercase text-sm shadow-gray-900">
-                Ver Carta
-              </button>
+              <div className="px-4">
+                <button className="bg-yellow-500 md:w-60 py-3 my-12 w-3/4 rounded-sm shadow-md uppercase text-sm shadow-gray-900">
+                  Ver Carta
+                </button>
+              </div>
             </div>
           </div>
           <div className="lg:col-span-5">
