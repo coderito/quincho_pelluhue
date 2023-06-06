2a2d6c1452d783ebeaa90b9088c5f1783b55e466
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-06-05 20:30:04 -0400

    agregando seccion video

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 import Hero from "./components/Hero"
 import GalleryFoot from "./components/GalleryFoot"
 import UbicacionRestaurante from "./components/UbicacionRestaurante"
+import VideoComponent from "./components/VideoComponent"
 
 export default function Home() {
   return (
     <>
       <Hero />
       <GalleryFoot />
-      <UbicacionRestaurante />     
+      <VideoComponent />  
+      <UbicacionRestaurante />   
     </>
   )
 }
