a4ab90d9006da13d3737f7b9b6fbfa22307cbad6
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-06-13 19:40:44 -0400

    agregando iconos fixed

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,53 @@ import Hero from "./components/Hero"
 import GalleryFoot from "./components/GalleryFoot"
 import UbicacionRestaurante from "./components/UbicacionRestaurante"
 import VideoComponent from "./components/VideoComponent"
+import { FaFacebook, FaInstagram, FaTwitter, FaWhatsapp } from 'react-icons/fa';
+
 
 export default function Home() {
   return (
     <>
+    <div className="hidden md:flex fixed top-32 flex-col gap-5 right-8 z-50 text-black">
+    <a
+      href="https://www.instagram.com/tu_usuario_de_instagram/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className=""
+    >
+      <FaInstagram size={24} />
+    </a>
+    <a
+      href="https://www.instagram.com/tu_usuario_de_instagram/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className=""
+    >
+      <FaFacebook size={24} />
+    </a>
+    <a
+      href="https://www.instagram.com/tu_usuario_de_instagram/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className=""
+    >
+      <FaWhatsapp size={24} />
+    </a>
+    <a
+      href="https://www.instagram.com/tu_usuario_de_instagram/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className=""
+    >
+      <FaTwitter size={24} />
+    </a>
+    </div>
+    
+    
       <Hero />
       <GalleryFoot />
       <VideoComponent />  
       <UbicacionRestaurante />   
+
     </>
   )
 }
