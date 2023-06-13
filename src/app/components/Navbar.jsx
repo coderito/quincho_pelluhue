76193c15a62e636bbe877fdf33441e847f4102e9
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-06-13 19:41:32 -0400

    modificando altura logo

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -25,28 +25,29 @@ export default function Navbar() {
   const pathname = usePathname();
 
   const estilosNavegacion =
-    "block hover:underline transition duration-150 ease-in-out cursor-pointer py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent hover:text-orange-400 md:p-0";
+    "block hover:underline transition duration-150 ease-in-out cursor-pointer py-2 pl-3 pr-4 text-white rounded md:bg-transparent hover:text-orange-400 md:p-0";
 
   return (
     <>
       <nav className="bg-black border-gray-200 fixed w-full z-50">
-        <div className="max-w-screen-lg flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="md:px-10 flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://flowbite.com/" className="flex items-center">
             <img
               src="img/icon/logo.png"
-              className="h-12 w-12 mr-3 bg-white rounded-full"
+              className="h-14 md:h-20 mr-3 bg-white rounded-full"
               alt="Flowbite Logo"
             />
             <span className={`${lobster.className} self-center hover:text-orange-200
-             tracking-wide md:tracking-widest text-sm md:text-lg uppercase whitespace-nowrap text-white`}>
-              El Quincho Pelluhue
+             tracking-wide font-bold md:tracking-widest text-sm md:text-lg uppercase whitespace-nowrap text-white`}>
+              <span className=" text-orange-500">Restaurant</span> pelluhue
             </span>
+          
           </a>
           <button
             data-collapse-toggle="navbar-default"
             type="button"
             onClick={handleMenu}
-            className="inline-flex items-center p-2 ml-3 text-md text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 ml-3 text-md text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
             aria-controls="navbar-default"
             aria-expanded="false"
           >
