fe902533b6584906b11562625c088e6fcdcccc64
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-06-14 23:27:28 -0400

    agregando pt-4

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className=" bottom-0 left-0 z-20 w-full p-4 mt-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6">
+    <footer className="bottom-0 left-0 z-20 w-full p-4 pt-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6">
       <span className="text-xs text-gray-500 sm:text-center dark:text-gray-400">
         © 2023{" "}
         <a href="https://flowbite.com/" className="hover:underline">
