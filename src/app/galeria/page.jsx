96d0593d99ab38df80de4e794eb9f0570dd13808
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-06-28 22:37:55 -0400

    creando galeria de restaurante

--- src/app/galeria/page.jsx
+++ src/app/galeria/page.jsx
@@ -1,7 +1,24 @@
+import { Alfa_Slab_One } from "next/font/google";
+
+const alfa_slab_one = Alfa_Slab_One({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 export default function Galeria() {
     return (
-      <div className="container pt-20">
-        <h1>Galeria</h1>
+      <div className="pt-28  lg:pt-32  h-full  bg-[url('/img/inicio/bg.jpg')]">
+      <div className="max-w-screen-xl h-screen mx-auto px-5 xl:px-0">
+        <h2
+          className={`${alfa_slab_one.className} text-6xl tracking-wider mb-12 text-orange-600`}
+        >
+          Galeria del restaurant
+        </h2>
+        <article>
+          
+
+        </article>
       </div>
+    </div>
     )
 }
