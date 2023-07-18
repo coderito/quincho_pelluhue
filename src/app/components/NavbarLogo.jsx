a2993274f0ed1d139827d9f80df0fd47b1f8757f
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-18 01:21:58 -0400

    agregando icono al medio del navbar

---
+++ src/app/components/NavbarLogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavbarLogo = () => {
+  return (
+    <div className='bg-black border-gray-200 flex items-center justify-center py-3'>
+        <img src="img/inicio/logo-header.png" alt="logito" width={300} height={62}/>
+    </div>
+  )
+}
+
+export default NavbarLogo
